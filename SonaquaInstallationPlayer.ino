6ab371087eb461b845de0be1e97e680bae6653a9
Scott Kildall <scott@kildall.com>
2017-06-19 17:05:05 +0700

    getting there....

--- SonaquaInstallationPlayer.ino
+++ SonaquaInstallationPlayer.ino
@@ -9,34 +9,52 @@
  *  https://hackaday.io/project/7008-fly-wars-a-hackers-solution-to-world-hunger/log/24646-three-dollar-ec-ppm-meter-arduino
  *  
  *  Uses the Tone library to manage multiple speakers
+ *  
+ *  Adafruit Backpack for visual debugging and feedback, the pins used are:
+ *  5V out (red wire), Ground (black wire), SDA (green) and SCL (yellow)
+ *
+ *  MSTimer code is my own, feel free to use/copy/modify
+ *
+ *
+ *
  */
+ 
 #include "Tone.h"
 #include "MSTimer.h"
 #include "Adafruit_LEDBackpack.h"
 
 //-- conditional defines
-#define LED_TEST          // will just loop LED on pins 52 (ground), 53 (+)
+//#define LED_TEST          // will just loop LED on pins 52 (ground), 53 (+)
 //#define EC_TEST             // will test EC on pins A0 (probe), 7 (EC Power)
 //#define TONE_TEST         // go through each, test the tones (maybe add a button for this...)
-
+#define CHANNEL_TEST      // will test LEDS, speakers and EC for each channel, progressively
+#define NO_TONE
 
 #define RANDOM_CHANCE (10)    // out of 1000
-#define NUM_ACTIVE_CHANNELS (5)
 
 // You can declare the tones as an array
-const int numChannels = 5;
+const int numChannels = 4;
 Tone tonePlayer[numChannels];
+Tone lineOut;
 
-const int startSeakerPin = 8;
 
+const int startLEDPositivePin = 31;       // these will be numbered 31, 33, 35, 37, 39
+const int startLEDGroundPin = 30;         // these will be numbered 30, 32, 34, 36, 38
+
+const int startSpeakerPositivePin = 43;   // these will be numbered 43, 45, 47, 49, 51
+const int startSpeakerGroundPin = 42;     // these will be numbered 42, 44, 46, 48, 50
+
+const int lineOutPositivePin = 9;
+const int lineOutGroundPin = 8;
+
+const int startPowerPin = 6;            // these will be numbered 6, 5, 4, 3, 2, descending order due to bridge
+const int startECPin = A0;              // these will be numbered A0, A1, A3
+
+const int ecValueNoPlayThreshhold = 950;    // value under which we will not play a tone
  //#define ECPin (A0)    
 
 boolean isOn[numChannels];
 
-// these need to proceed numerically, e.g. A0, A1, A2...
-const int startECPin (A0);
-
-const int startPowerPin (30);      // also numerically
 
 
 unsigned int rawEC[numChannels];
@@ -69,17 +87,19 @@ Serial.println("---------- Sonaqua Installation Player ----------" );
 
   
  for( int i = 0; i < numChannels; i++ ) {
-  tonePlayer[i].begin(startSeakerPin + i);
+  tonePlayer[i].begin(startSpeakerPositivePin + (i*2) );
   isOn[i] = false;
  }
 
+  lineOut.begin(lineOutPositivePin);
+
 
    for( int i = 0; i < numChannels; i++ )
-    pinMode(startECPin + i,INPUT);
+    pinMode(startECPin + (i*2),INPUT);
 
    //Setting pins for sourcing current
   for( int i = 0; i < numChannels; i++ ) 
-    pinMode(startPowerPin + i,OUTPUT);
+    pinMode(startPowerPin + (i*2),OUTPUT);
 
 
   // Initialize times
@@ -92,7 +112,7 @@ Serial.println("---------- Sonaqua Installation Player ----------" );
     
   matrix.begin(0x70);
  
-  matrix.print(9999, DEC);
+  matrix.print(7777, DEC);
   matrix.writeDisplay();
 
 
@@ -107,10 +127,43 @@ Serial.println("---------- Sonaqua Installation Player ----------" );
   pinMode(ECTestPowerPin,OUTPUT);                // set pin for sourcing current
   digitalWrite(LEDTestGroundPin, LOW);
 #endif
+
+  // LED outputs
+  for( int i = 0; i < numChannels; i++) {
+    pinMode(startLEDPositivePin + (i*2), OUTPUT);
+    digitalWrite(startLEDPositivePin, LOW);
+
+    pinMode(startSpeakerPositivePin + (i*2), OUTPUT);
+    pinMode(lineOutPositivePin, OUTPUT);
+  }
+
+  // All ground pins
+  for( int i = 0; i < numChannels; i++ ){
+    pinMode(startLEDGroundPin + (i*2), OUTPUT);
+    digitalWrite(startLEDGroundPin, LOW);
+
+    pinMode(startSpeakerGroundPin + (i*2), OUTPUT);
+    digitalWrite(startSpeakerGroundPin, LOW);
+
+    pinMode(lineOutGroundPin, OUTPUT);
+    digitalWrite(lineOutGroundPin, LOW);
+  }
+
+  // EC pins
+  for( int i = 0; i < numChannels; i++ )
+    pinMode( getPowerPin(i), OUTPUT );
+
+   for( int i = 0; i < numChannels; i++ )
+    pinMode(getECPin(i),INPUT);
 }
 
 void loop(void)
 {
+#ifdef CHANNEL_TEST
+  channelTest();
+  return;
+#endif
+
 #ifdef EC_TEST
   int rawEC = getEC(ECTestPowerPin,ECTestProbePin);
   
@@ -137,6 +190,7 @@ void loop(void)
   delay(delayTime);
   return;
 #endif
+
 
 
 
@@ -222,5 +276,58 @@ unsigned int getEC(int powerPin, int probePin){
   digitalWrite(powerPin,LOW);
  
  return raw;
+}
+
+//-- go through each channel, show LEDs, display on backpack
+void channelTest() {
+   int delayTime = 2000;
+   
+  // LED outputs
+  for( int i = 0; i < numChannels; i++ ) {
+    matrix.print(i, DEC);
+    matrix.writeDisplay();
+    delay(500);
+
+    int ecValue = getEC( getPowerPin(i), getECPin(i) );
+    matrix.print(ecValue, DEC);
+    matrix.writeDisplay();
+    
+    digitalWrite(startLEDPositivePin + (i*2), HIGH);
+    delay(delayTime);
+
+    int toneValue = random(25,200);
+    #ifndef NO_TONE
+      tonePlayer[i].play(toneValue);
+      lineOut.play(toneValue);
+    #endif
+     
+    digitalWrite(startLEDPositivePin + (i*2), LOW);
+    delay(delayTime);
+
+    tonePlayer[i].stop();
+    lineOut.stop();
+  }
+}
+
+//-- power are digital pins, start at 6, goes down from there
+int getPowerPin(int channelNum) {
+  return (startPowerPin - channelNum);
+}
+
+int getECPin(int channelNum) {
+  if( channelNum == 0 )
+    return A0;
+  else if( channelNum == 1 )
+    return A1;
+  return (startECPin + channelNum);
+}
+
+int makeToneFromEC(int ecValue) {
+  if( ecValue < ecValueNoPlayThreshhold )
+      return 0;
+      
+  int toneValue = ecValue/2;
+
+  return toneValue;
 }
 
